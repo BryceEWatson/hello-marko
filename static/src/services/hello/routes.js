bb004bcf009f86f85d6cb2ce86ac907fc591dcf5
Bryce Watson <bryceewatson@gmail.com>
2016-02-27 16:54:26 -0800

    removing static folder

--- static/src/services/hello/routes.js
+++
@@ -1,8 +0,0 @@
-$rmod.def("/src/services/hello/routes", function(require, exports, module, __filename, __dirname) { module.exports = [
-    {
-        name: 'getWelcomeMsg',
-        method: 'GET',
-        path: '/api/getWelcomeMsg'
-    }
-];
-});
